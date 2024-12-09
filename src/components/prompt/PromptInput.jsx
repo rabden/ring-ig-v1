d538befd2d7b388e7849d06db215f51de7a77d58
rabden <163085056+rabden@users.noreply.github.com>
2024-12-09 11:50:32 +0600

    hgcvkjgc

--- src/components/prompt/PromptInput.jsx
+++ src/components/prompt/PromptInput.jsx
@@ -70,7 +70,7 @@ const PromptInput = ({
           onClick={handleGenerate}
           disabled={!value?.length || !hasEnoughCredits}
         >
-          Create
+          Generate
           <ArrowRight className="ml-2 h-4 w-4" />
         </Button>
       </div>
