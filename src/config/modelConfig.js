e9cefc5d3d5f822fd05aaf2a9097cff00318115f
rabden <163085056+rabden@users.noreply.github.com>
2024-12-16 10:45:36 +0600

    new models added

--- src/config/modelConfig.js
+++ src/config/modelConfig.js
@@ -1,6 +1,6 @@
 export const modelConfig = {
   turbo: {
-    name: "Ring.1 turbo",
+    name: "SD-3.5Large-turbo",
     category: "General",
     apiUrl: "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-3.5-large-turbo",
     qualityLimits: ["HD"],
@@ -10,7 +10,7 @@ export const modelConfig = {
     image: "/models/turbo.png"
   },
   flux: {
-    name: "Ring.1",
+    name: "Flux.1 Schnell",
     category: "General",
     apiUrl: "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell",
     qualityLimits: null,
@@ -20,23 +20,113 @@ export const modelConfig = {
     image: "/models/flux.png"
   },
   fluxDev: {
-    name: "Ring.1 hyper",
+    name: "FLx.1 Dev",
     category: "General",
     apiUrl: "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
     qualityLimits: null,
-    isPremium: true,
+    isPremium: false,
     promptSuffix: null,
     tagline: "High-quality artistic creations",
     image: "/models/fluxdev.png"
   },
+  fastReal: {
+    name: "Realism turbo",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/prithivMLmods/Simple-Doodle-SD3.5-Turbo",
+    qualityLimits: ["HD"],
+    isPremium: false,
+    promptSuffix: null,
+    tagline: "make realistic images fast",
+    image: "/models/ultra.png"
+  },
+  Illustturbo: {
+    name: "Illustration turbo",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/prithivMLmods/Simple-Doodle-SD3.5-Turbo",
+    qualityLimits: ["HD"],
+    isPremium: false,
+    promptSuffix: null,
+    tagline: "make simple illustrations fast",
+    image: "/models/ultra.png"
+  },
+  render3d: {
+    name: "3D Render",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/goofyai/3d_render_style_xl",
+    qualityLimits: ["HD"],
+    isPremium: false,
+    promptSuffix: null,
+    tagline: "render 3D images",
+    image: "/models/ultra.png"
+  },
+  anime: {
+    name: "Anime-Xl",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/Nishitbaria/Anime-style-flux-lora-Large",
+    qualityLimits: null,
+    isPremium: false,
+    promptSuffix: null,
+    tagline: "Flwaless Anime style images",
+    image: "/models/ultra.png"
+  },
+  midjourney: {
+    name: "Midjourney",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/strangerzonehf/Flux-Midjourney-Mix2-LoRA",
+    qualityLimits: null,
+    isPremium: false,
+    promptSuffix: null,
+    tagline: "Latest version of Midjourney for free",
+    image: "/models/ultra.png"
+  },
+  Isometric: {
+    name: "Isometric",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/strangerzonehf/Flux-Isometric-Site-LoRA",
+    qualityLimits: null,
+    isPremium: false,
+    promptSuffix: null,
+    tagline: "Isometric style, build your world",
+    image: "/models/ultra.png"
+  },
+  outfitGenerator: {
+    name: "Outfit Generator",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/prithivMLmods/Teen-Outfit",
+    qualityLimits: null,
+    isPremium: false,
+    promptSuffix: null,
+    tagline: "Make cool outfits and cloths",
+    image: "/models/ultra.png"
+  },
+  colorChaos: {
+    name: "Color Chaos",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/prithivMLmods/Digital-Chaos-Flux-LoRA",
+    qualityLimits: null,
+    isPremium: false,
+    promptSuffix: null,
+    tagline: "Chaos of Colors to make modern art",
+    image: "/models/ultra.png"
+  },
+  aura: {
+    name: "Aura 999+",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/prithivMLmods/Aura-9999",
+    qualityLimits: null,
+    isPremium: false,
+    promptSuffix: null,
+    tagline: "gives your creations some aura",
+    image: "/models/ultra.png"
+  },
   ultra: {
-    name: "Ultra",
+    name: "Flux.1 Pro",
     category: "General",
     apiUrl: "https://api-inference.huggingface.co/models/strangerzonehf/Flux-Ultimate-LoRA-Collection",
     qualityLimits: null,
     isPremium: true,
     promptSuffix: null,
-    tagline: "Ultimate quality and detail",
+    tagline: "A merge of multiple models",
     image: "/models/ultra.png"
   },
   animeNsfw: {
