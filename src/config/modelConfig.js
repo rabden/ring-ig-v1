df4a95ce9c120b7b3be520deeb0c08fe70a9be1e
rabden <163085056+rabden@users.noreply.github.com>
2024-12-17 10:51:32 +0600

    ahbc

--- src/config/modelConfig.js
+++ src/config/modelConfig.js
@@ -19,6 +19,16 @@ export const modelConfig = {
     tagline: "Balanced speed and quality",
     image: "/models/flux.png"
   },
+  sd35l: {
+    name: "SD-3.5Large",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-3.5-large",
+    qualityLimits: ["HD"],
+    isPremium: false,
+    promptSuffix: null,
+    tagline: "Latest Stable diffusion model",
+    image: "/models/turbo.png"
+  },
   fluxDev: {
     name: "FLx.1 Dev",
     category: "General",
@@ -26,18 +36,18 @@ export const modelConfig = {
     qualityLimits: null,
     isPremium: false,
     promptSuffix: null,
-    tagline: "High-quality artistic creations",
+    tagline: "Flux dev by black forest labs",
     image: "/models/fluxdev.png"
   },
   fastReal: {
     name: "Realism turbo",
     category: "General",
-    apiUrl: "https://api-inference.huggingface.co/models/prithivMLmods/SD3.5-Large-Turbo-HyperRealistic-LoRA",
+    apiUrl: "https://api-inference.huggingface.co/models/prithivMLmods/SD3.5-Large-Photorealistic-LoRA",
     qualityLimits: ["HD"],
     isPremium: false,
-    promptSuffix: ", hyper realistic",
+    promptSuffix: ", photography, photorealistic",
     tagline: "make realistic images fast",
-    image: "/models/ultra.png"
+    image: "https://i.ibb.co.com/JrYsbmT/SD3.webp"
   },
   Illustturbo: {
     name: "Illustration turbo",
@@ -47,37 +57,57 @@ export const modelConfig = {
     isPremium: false,
     promptSuffix: ", Simple Doodle",
     tagline: "make simple illustrations fast",
-    image: "/models/ultra.png"
+    image: "https://i.ibb.co.com/Mhk3M6v/SD1.webp"
   },
   render3d: {
-    name: "3D Render",
+    name: "3D Render XL",
     category: "General",
     apiUrl: "https://api-inference.huggingface.co/models/goofyai/3d_render_style_xl",
     qualityLimits: ["HD"],
     isPremium: false,
     promptSuffix: ", 3d render",
     tagline: "render 3D images",
-    image: "/models/ultra.png"
+    image: "https://i.ibb.co.com/sbmM5mp/3d-style-2.jpg"
   },
   anime: {
-    name: "Anime-Xl",
+    name: "Anime XL",
     category: "General",
     apiUrl: "https://api-inference.huggingface.co/models/Nishitbaria/Anime-style-flux-lora-Large",
     qualityLimits: null,
     isPremium: false,
     promptSuffix: ", anm",
     tagline: "Flwaless Anime style images",
-    image: "/models/ultra.png"
+    image: "https://i.ibb.co.com/k2YdjZK/images-example-7y3r4uk1q.jpg"
+  },
+  anime2xl: {
+    name: "Anime XL Fast",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/cagliostrolab/animagine-xl-3.0",
+    qualityLimits: ["HD"],
+    isPremium: false,
+    promptSuffix: ", anime",
+    tagline: "Anime style images make fast",
+    image: "https://i.ibb.co.com/bgYbVQJ/Euv-INv-Bs-CKZQusp-ZHN-u-F.png"
   },
   midjourney: {
-    name: "Midjourney",
+    name: "Midjourney-V6",
     category: "General",
     apiUrl: "https://api-inference.huggingface.co/models/strangerzonehf/Flux-Midjourney-Mix2-LoRA",
     qualityLimits: null,
     isPremium: false,
     promptSuffix: ", MJ v6",
     tagline: "Latest version of Midjourney for free",
-    image: "/models/ultra.png"
+    image: "https://i.ibb.co.com/8PnDLkf/1.png"
+  },
+  sketchart: {
+    name: "Sketch Art",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/Datou1111/shou_xin",
+    qualityLimits: null,
+    isPremium: false,
+    promptSuffix: ", shou_xin, pencil sketch",
+    tagline: "sketch art pencil and colors",
+    image: "https://i.ibb.co.com/P4xLs4W/img-00282.png"
   },
   vertorArt: {
     name: "Vector Art",
@@ -87,7 +117,7 @@ export const modelConfig = {
     isPremium: false,
     promptSuffix: ", v3ct0r, vector",
     tagline: "Create Vector arts",
-    image: "/models/ultra.png"
+    image: "https://i.ibb.co.com/wKszPV2/images-example-ylmvpzdqk.jpg"
   },
   Isometric: {
     name: "Isometric",
@@ -97,7 +127,17 @@ export const modelConfig = {
     isPremium: false,
     promptSuffix: ", Isometric Building",
     tagline: "Isometric style, build your world",
-    image: "/models/ultra.png"
+    image: "https://i.ibb.co.com/84bxty7/2.png"
+  },
+  handwriting: {
+    name: "Handwriting",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/fofr/flux-handwriting",
+    qualityLimits: null,
+    isPremium: false,
+    promptSuffix: ", HWRIT handwriting",
+    tagline: "Real lookin handwriting images",
+    image: "https://i.ibb.co.com/mFPgMkj/0041358a-ee46-4cab-85f5-108c4f09b729.jpg"
   },
   outfitGenerator: {
     name: "Outfit Generator",
@@ -107,7 +147,7 @@ export const modelConfig = {
     isPremium: false,
     promptSuffix: ", Teen Outfit",
     tagline: "Make cool outfits and cloths",
-    image: "/models/ultra.png"
+    image: "https://i.ibb.co.com/MCWQZBN/TO3.png"
   },
   colorChaos: {
     name: "Color Chaos",
@@ -117,7 +157,27 @@ export const modelConfig = {
     isPremium: false,
     promptSuffix: ", Digital Chaos",
     tagline: "Chaos of Colors to make modern art",
-    image: "/models/ultra.png"
+    image: "https://i.ibb.co.com/TgcCsdf/HDRDC2.webp"
+  },
+  pixelArt: {
+    name: "Pixel Art",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/nerijs/pixel-art-3.5L",
+    qualityLimits: ["HD"],
+    isPremium: false,
+    promptSuffix: ", pixel art",
+    tagline: "CModern Pixel art",
+    image: "https://i.ibb.co.com/DkdtLrG/Comfy-UI-00047.png"
+  },
+  iconkit: {
+    name: "Icon Kit",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/strangerzonehf/Flux-Icon-Kit-LoRA",
+    qualityLimits: null,
+    isPremium: false,
+    promptSuffix: ", Icon kit",
+    tagline: "3d Icons",
+    image: "https://i.ibb.co.com/cxqCCnf/1-1.png"
   },
   aura: {
     name: "Aura 9999+",
@@ -127,7 +187,17 @@ export const modelConfig = {
     isPremium: false,
     promptSuffix: ", Aura 9999",
     tagline: "gives your creations some aura",
-    image: "/models/ultra.png"
+    image: "https://i.ibb.co.com/NNWjs4d/A3.png"
+  },
+  disney: {
+    name: "Disney",
+    category: "General",
+    apiUrl: "https://api-inference.huggingface.co/models/Keltezaa/all-disney-princess-xl-lora-model-from-ralph-breaks-the-internet",
+    qualityLimits: null,
+    isPremium: false,
+    promptSuffix: nll,
+    tagline: "Disney princes, use name",
+    image: "https://i.ibb.co.com/54bxzMk/4058459.jpg"
   },
   ultra: {
     name: "Flux.1 Pro",
@@ -142,12 +212,12 @@ export const modelConfig = {
   nsfwMaster: {
     name: "Real",
     category: "NSFW",
-    apiUrl: "https://api-inference.huggingface.co/models/pimpilikipilapi1/NSFW_master",
+    apiUrl: "https://api-inference.huggingface.co/models/aifeifei798/sldr_flux_nsfw_v2-studio",
     qualityLimits: null,
     isPremium: false,
     promptSuffix: ", Nsfw, naked,",
     tagline: "NSFW generation",
-    image: "/models/nsfw.png"
+    image: "https://i.ibb.co.com/qm9ZjV4/b36e71ed-c142-49c4-8ba5-4f3a57bd6ae9.jpg"
   },
   animeNsfw: {
     name: "Anime",
@@ -157,6 +227,16 @@ export const modelConfig = {
     isPremium: false,
     promptSuffix: null,
     tagline: "Anime-style NSFW generation",
-    image: "/models/anime.png"
+    image: "https://i.ibb.co.com/Tt1gwLG/1730283256633.jpg"
+  },
+  nudephotography: {
+    name: "Nude Photography",
+    category: "NSFW",
+    apiUrl: "https://api-inference.huggingface.co/models/Keltezaa/Prof_Nude_photography_v3_flux",
+    qualityLimits: null,
+    isPremium: false,
+    promptSuffix: ", Nude photograpy, naked",
+    tagline: "Nude models for Photoshoot",
+    image: "https://i.ibb.co.com/GW2LDP1/example-20kld8mba.jpg"
   }
 };
